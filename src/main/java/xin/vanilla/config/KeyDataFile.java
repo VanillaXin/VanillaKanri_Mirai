1ac0f3a4181b5f1d3fef5f05506a7023f346f595
Tsuki <tsukimaaii@gmail.com>
2024-06-12 17:07:29 +0800

    chore 1.关键词改用配置文件存储

--- src/main/java/xin/vanilla/config/KeyDataFile.java
+++ src/main/java/xin/vanilla/config/KeyDataFile.java
@@ -4,7 +4,7 @@ import net.mamoe.mirai.console.data.SerializerAwareValue;
 import net.mamoe.mirai.console.data.java.JavaAutoSavePluginData;
 import xin.vanilla.entity.data.KeyData;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 关键词数据
@@ -14,14 +14,14 @@ public class KeyDataFile extends JavaAutoSavePluginData {
     /**
      * 关键词
      */
-    public SerializerAwareValue<ArrayList<KeyData>> key = typedValue("keyword", createKType(ArrayList.class, createKType(KeyData.class)));
+    public SerializerAwareValue<List<KeyData>> key = typedValue("keyword", createKType(List.class, createKType(KeyData.class)));
 
 
     public KeyDataFile() {
         super("key_data");
     }
 
-    public ArrayList<KeyData> getKey() {
+    public List<KeyData> getKey() {
         return key.get();
     }
 }
